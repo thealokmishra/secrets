4db01a96db1b8aaf1a1679e99eb7dc6fab78dbd4
Alok Mishra <mishraalok2130@gmail.com>
2026-01-16 11:35:44 +0530

    google login fix

--- index.js
+++ index.js
@@ -206,7 +206,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      // callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
